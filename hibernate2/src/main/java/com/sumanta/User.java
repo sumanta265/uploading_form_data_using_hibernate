010496dd901b3d9f570db4d7ed96ae65edb97895
sumanta265 <54164868+sumanta265@users.noreply.github.com>
2020-09-22 13:02:57 +0530

    Add files via upload

---
+++ hibernate2/src/main/java/com/sumanta/User.java
@@ -0,0 +1,46 @@
+package com.sumanta;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user123")
+public class User {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;  
+	
+	private String name,password,email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}  
+	
+	
+	
+}
---
+++ hibernate2/src/main/webapp/register.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<%@page import="com.sumanta.UserDao"%>  
+<jsp:useBean id="obj" class="com.sumanta.User">  
+</jsp:useBean>  
+<jsp:setProperty property="*" name="obj"/>  
+  
+<%  
+int i=UserDao.register(obj);  
+if(i>0)  
+out.print("You are successfully registered");  
+  
+%>  
+</body>
+</html>
